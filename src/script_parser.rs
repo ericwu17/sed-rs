4d353656613008fab007cb9c246b7e668ec4fd95
Eric <eric.dianhao.wu@gmail.com>
2023-03-29 00:02:40 -0400

    refactor

--- src/script_parser.rs
+++ src/script_parser.rs
@@ -11,69 +11,66 @@ pub enum Command {
 }
 
 pub fn parse_script(script: &str) -> Result<Command, ()> {
+    let mut script_iterator = script.chars();
+
     if script.is_empty() {
         return Err(());
     }
-    match script.chars().next() {
-        None => {
-            Err(())
-        }
-        Some('s') => {
-            let delimiter: char = script.chars().nth(1).ok_or(())?;
 
-            let mut remainder = script.chars().skip(2);
-            let mut regexp = String::new();
-            let mut replacement = String::new();
+    let first_char = script_iterator.next().ok_or(())?;
 
-            loop {
-                // match the search expression
-                let c = remainder.next();
-                match c {
-                    Some(character) => {
-                        if character == delimiter {
-                            break;
-                        }
-                        regexp += &character.to_string();
-                    }
-                    None => return Err(()),
+    let delimiter: char = script_iterator.next().ok_or(())?;
+
+    let mut regexp = String::new();
+    let mut replacement = String::new();
+
+    loop {
+        // match the search expression
+        let c = script_iterator.next();
+        match c {
+            Some(character) => {
+                if character == delimiter {
+                    break;
                 }
+                regexp += &character.to_string();
             }
-            loop {
-                // match the replacement expression
-                let c = remainder.next();
-                match c {
-                    Some(character) => {
-                        if character == delimiter {
-                            break;
-                        }
-                        replacement += &character.to_string();
-                    }
-                    None => return Err(()),
+            None => return Err(()),
+        }
+    }
+    loop {
+        // match the replacement expression
+        let c = script_iterator.next();
+        match c {
+            Some(character) => {
+                if character == delimiter {
+                    break;
                 }
+                replacement += &character.to_string();
             }
-            let mut max_replacements: Option<usize> = Some(1);
-
-            // match additional flags (only options are 'g' for global or a whole number)
-            let remainder: String = remainder.collect();
-            if remainder == "g" {
-                max_replacements = None;
-            } else if let Ok(n) = remainder.parse::<usize>() {
-                max_replacements = Some(n);
-            } else if !remainder.is_empty() {
-                return Err(());
-            }
-
-            let regexp = interpret_escaped_string(&regexp).map_err(|_| ())?;
-            let replacement = interpret_escaped_string(&replacement).map_err(|_| ())?;
-
-            Ok(Command::Substitute {
-                regexp,
-                replacement,
-                max_replacements,
-            })
+            None => return Err(()),
         }
-        _ => {
-            Err(())
-        }
+    }
+    let mut max_replacements: Option<usize> = Some(1);
+
+    // match additional flags (only options are 'g' for global or a whole number)
+    let remainder: String = script_iterator.collect();
+    if remainder == "g" {
+        max_replacements = None;
+    } else if let Ok(n) = remainder.parse::<usize>() {
+        max_replacements = Some(n);
+    } else if !remainder.is_empty() {
+        return Err(());
+    }
+
+    let regexp = interpret_escaped_string(&regexp).map_err(|_| ())?;
+    let replacement = interpret_escaped_string(&replacement).map_err(|_| ())?;
+
+    match first_char {
+        's' => Ok(Command::Substitute {
+            regexp,
+            replacement,
+            max_replacements,
+        }),
+        _ => Err(()),
     }
 }
